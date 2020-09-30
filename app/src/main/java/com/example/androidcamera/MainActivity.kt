abd2c4677fa400cbd106f209d7b143155a98d7b6
Fatih Can <ybsfatihcan@gmail.com>
2020-09-30 18:49:14 +0300

    sample

---
+++ app/src/main/java/com/example/androidcamera/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.androidcamera
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnTakePhoto.setOnClickListener {
+            val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            filePhoto = getPhotoFile(FILE_NAME)
+
+
+            val providerFile =FileProvider.getUriForFile(this,"com.example.androidcamera.fileprovider", filePhoto)
+            takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerFile)
+            if (takePhotoIntent.resolveActivity(this.packageManager) != null){
+            startActivityForResult(takePhotoIntent, REQUEST_CODE)
+            }else {
+                Toast.makeText(this,"Camera could not open", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getPhotoFile(fileName: String): File {
+        val directoryStorage = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(fileName, ".jpg", directoryStorage)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            val takenPhoto = BitmapFactory.decodeFile(filePhoto.absolutePath)
+            viewImage.setImageBitmap(takenPhoto)
+        }else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+}
+
+private const val REQUEST_CODE = 13
+private lateinit var filePhoto: File
+private const val FILE_NAME = "photo.jpg"
